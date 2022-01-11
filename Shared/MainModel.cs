328b8129a808f82eae445f0444377037a0889fda
IviweMalotana <53525294+IviweMalotana@users.noreply.github.com>
2022-01-11 16:46:40 +0200

    Add project files.

---
+++ Shared/MainModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace XebecPortal.UI.Shared
+{
+    public class MainModel
+    {
+        public bool isDeveloper { get; set; } = false;
+        public bool isHR { get; set; } = false;
+        public bool isApplicant { get; set; } = true;
+    }
+}
