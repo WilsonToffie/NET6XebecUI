444834504d5b24ee8899da5e78de7d9b2e7019c2
DBailie <DanielB@nebula.co.za>
2022-01-25 09:34:57 +0200

    Daniel's commits

---
+++ Shared/CustomQuestion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace XebecPortal.UI.Shared
+{
+    public class CustomQuestion
+    {
+        public int Id { get; set; }
+
+        public string questionDescription { get; set; }
+    }
+}
---
+++ Shared/Job.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace XebecPortal.UI.Shared
+{
+    public class Job
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string Location { get; set; }
+
+        public DateTime DueDate { get; set; }
+
+        public DateTime CreationDate { get; set; }
+
+        public double Compensation { get; set; }
+
+        public int MinimumExperience { get; set; }
+
+        public string Department { get; set; }
+    }
+}
