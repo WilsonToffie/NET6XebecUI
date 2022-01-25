7b3509e6abf41b380842241ec5478814f6449681
TebohoNebula <90898070+TebohoNebula@users.noreply.github.com>
2022-01-25 08:48:40 +0200

    Search and Show Details Functionality has been added

--- Pages/HR/HrDataModels.cs
+++ Pages/HR/HrDataModels.cs
@@ -16,4 +16,35 @@ namespace XebecPortal.UI.Pages.HR
         public string interview_comment { get; set; }
         public string phase { get; set; }
     }
+
+    public class Job
+    {
+        public int id { get; set; }
+        public string jobName { get; set; }
+        public string companyName { get; set; }
+        public string location { get; set; }
+        public string department { get; set; }
+        public string description { get; set; }
+        public string socialMedia { get; set; }
+        public DateTime dateAdvertised { get; set; }
+        public DateTime dateDue { get; set; }
+    }
+
+    public class MockLocation
+    {
+        public int id { get; set; }
+        public string location { get; set; }
+    }
+
+    public class MockDepartment
+    {
+        public int id { get; set; }
+        public string department { get; set; }
+    }
+
+    public class MockSocialMedia
+    {
+        public int id { get; set; }
+        public string socialMedia { get; set; }
+    }
 }
--- Pages/HR/JobPortal.razor.cs
+++ Pages/HR/JobPortal.razor.cs
@@ -2,24 +2,36 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using System.Net.Http.Json;
+using System.Threading.Tasks;
 using X.PagedList;
+using System.Text.Json;
 
 namespace XebecPortal.UI.Pages.HR
 {
     public partial class JobPortal
     {
         private bool changeForm;
+        private string searchJob;
         private List<int> pageNum = new List<int>();
-        private List<Job> jobList = new List<Job>();
+        private IList<Job> jobList = new List<Job>();
+        private IList<Job> jobListFilter = new List<Job>();
+        private Job displayJobDetail = new Job();
         private IPagedList<Job> jobPagedList = new List<Job>().ToPagedList();
+        private IList<MockDepartment> mockDepartments = new List<MockDepartment>();
+        private IList<MockLocation> mockLocations = new List<MockLocation>();
+        private IList<MockSocialMedia> mockSocialMedia = new List<MockSocialMedia>();
 
         protected override async Task OnInitializedAsync()
         {
             jobList = await httpClient.GetFromJsonAsync<List<Job>>("/mockData/jobMockData.json");
-            jobPagedList = jobList.ToPagedList(1, 17);
+            mockDepartments = await httpClient.GetFromJsonAsync<List<MockDepartment>>("/mockData/departmentMockData.json");
+            mockLocations = await httpClient.GetFromJsonAsync<List<MockLocation>>("/mockData/locationMockData.json");
+            mockSocialMedia = await httpClient.GetFromJsonAsync<List<MockSocialMedia>>("/mockData/socialmediaMockData.json");
+            jobListFilter = jobList;
+            jobPagedList = jobListFilter.ToPagedList(1, 17);
             pageNum.AddRange(Enumerable.Range(1, jobPagedList.PageCount));
+            displayJobDetail = jobListFilter.FirstOrDefault();
         }
 
         protected override Task OnAfterRenderAsync(bool firstRender)
@@ -38,21 +50,53 @@ namespace XebecPortal.UI.Pages.HR
             changeForm = value;
         }
 
-        private async Task DeleteData()
+        private async Task DeleteData(int id)
         {
-            if (!await jsRuntime.InvokeAsync<bool>("confirm", "Are You Certain You Want To Delete This Item?"))
-                return;
+            if (await jsRuntime.InvokeAsync<bool>("confirm", "Are You Certain You Want To Delete This Item?"))
+            {
+                jobList = jobList.Where(x => x.id != id).ToList();
+                var jsonFile = JsonSerializer.Serialize<List<Job>>(jobList.ToList());
+            }
         }
 
-        private async Task SaveData()
+        private async Task SaveData(Job value)
         {
-            if (!await jsRuntime.InvokeAsync<bool>("confirm", "Are You Certain You Want To Override This Item?"))
-                return;
+            if (await jsRuntime.InvokeAsync<bool>("confirm", "Are You Certain You Want To Override This Item?"))
+            {
+                var itemSearch = jobList.First(x => x.id == value.id);
+                var index = jobList.IndexOf(itemSearch);
+                jobList[index] = value;
+            }
         }
 
         private void PageListNav(int value)
         {
             jobPagedList = jobList.ToPagedList(value, 17);
+        }
+
+        private void SeachListJob(string value)
+        {
+            pageNum.Clear();
+
+            if (value != null && value != "" && value != " ")
+            {
+                jobListFilter = jobList.Where(x => $"{x.jobName} {x.companyName} {x.location}".Contains(value, StringComparison.OrdinalIgnoreCase)).ToList();
+                jobPagedList = jobListFilter.ToPagedList(1, 17);
+                pageNum.AddRange(Enumerable.Range(1, jobPagedList.PageCount));
+            }
+            else
+            {
+                jobListFilter = jobList;
+                jobPagedList = jobListFilter.ToPagedList(1, 17);
+                pageNum.AddRange(Enumerable.Range(1, jobPagedList.PageCount));
+            }
+
+            displayJobDetail = jobListFilter.FirstOrDefault();
+        }
+
+        private void DisplayJobDetail(int id)
+        {
+            displayJobDetail = jobListFilter.FirstOrDefault(x => x.id == id);
         }
     }
 }
