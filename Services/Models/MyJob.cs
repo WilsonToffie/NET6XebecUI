d77b02630ef56d0b2c17c33cbfbe5be2dbd1764c
stfeleti <stfeleti@gmail.com>
2022-01-27 08:51:58 +0200

    Fixed scrollbar

--- Services/Models/MyJob.cs
+++ Services/Models/MyJob.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace XebecPortal.UI.Services.Models
 {
@@ -11,5 +12,28 @@ namespace XebecPortal.UI.Services.Models
         public string Status { get; set; }
         public DateTime LastMoved { get; set; }
         public DateTime ApplicationDate { get; set; }
+
+        protected bool Equals(string other)
+        {
+            
+            return Position.ToLower().Contains(other.ToLower()) || Company.ToLower().Contains(other.ToLower()) ||
+                   Location.ToLower().Contains(other.ToLower()) || Phase.ToLower().Contains(other.ToLower()) ||
+                   Status.ToLower().Contains(other.ToLower()) ||
+                   LastMoved.ToString(CultureInfo.CurrentCulture).ToLower().Contains(other.ToLower()) || 
+                   ApplicationDate.ToString(CultureInfo.CurrentCulture).ToLower().Contains(other.ToLower());
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((MyJob) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Position, Company, Location, Phase, Status, LastMoved, ApplicationDate);
+        }
     }
 }
