854373c645419ee1ce3400f9768086d5dfd21ad9
Armand <armandvdberg56@gmail.com>
2022-02-02 14:45:02 +0200

    Armand(CreateJob with parameters)

--- Pages/HR/Models/HrDataModels.cs
+++ Pages/HR/Models/HrDataModels.cs
@@ -35,6 +35,52 @@ namespace XebecPortal.UI.Pages.HR
         public List<Application> Applications { get; set; }
     }
 
+    public class CreateJobPost
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Company { get; set; }
+        public decimal? Compensation { get; set; }
+        public int? MinimumExperience { get; set; }
+        public string Location { get; set; }
+        public string Department { get; set; }
+        public DateTime DueDate { get; set; }
+        public DateTime CreationDate { get; set; }
+        public JobTypeHelper JobType { get; set; }
+        public List<JobPlatformHelper> JobPlatforms { get; set; }
+        public List<ApplicationPhase> JobPhases { get; set; }
+        public List<FormQuestion> formQuestions { get; set; }
+    }
+
+    public class FormQuestion
+    {
+        public int id { get; set; }
+
+        public string question { get; set; }
+
+        public string answer { get; set; }
+
+        public int jobId { get; set; }
+
+        public string job { get; set; }
+    }
+
+    public class CustomQuestion
+    {
+        public int Id { get; set; }
+
+        public string questionDescription { get; set; }
+    }
+
+    class Developer
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Role { get; set; }
+        public string PasswordHash { get; set; }
+    }
+
     public class JobType
     {
         public int Id { get; set; }
@@ -52,14 +98,54 @@ namespace XebecPortal.UI.Pages.HR
         public JobType JobType { get; set; }
     }
 
+    public class ApplicationPhase
+    {
+        public int id { get; set; }
+
+        public string description { get; set; }
+    }
+
+    public class AppUser
+    {
+        public int id { get; set; }
+
+        public string email { get; set; }
+
+        public string role { get; set; }
+
+        public string passwordhash { get; set; }
+
+        public string name { get; set; }
+
+        public string surname { get; set; }
+
+        public string key { get; set; }
+
+        public bool registered { get; set; }
+
+        public int linkVisits { get; set; }
+    }
+
     public class JobPlatformHelper
     {
-
+        public int Id { get; set; }
+        //Foreign Key: Job
+        public int JobId { get; set; }
+        public Job Job { get; set; }
+        //Foreign Key: JobPlatform
+        public int JobPlatformId { get; set; }
+        public JobPlatform JobPlatform { get; set; }
     }
 
     public class JobApplicationPhase
     {
-
+        public int Id { get; set; }
+        //Foreign Key: Job
+        public int JobId { get; set; }
+        public Job Job { get; set; }
+        //Foreign Key: ApplicationPhase
+        public int ApplicationPhaseId { get; set; }
+        public ApplicationPhase ApplicationPhase { get; set; }
     }
 
     public class Application
@@ -83,5 +169,11 @@ namespace XebecPortal.UI.Pages.HR
     {
         public int id { get; set; }
         public string socialMedia { get; set; }
+    }
+
+    public class JobPlatform
+    {
+        public int id { get; set; }
+        public string platformName { get; set; }
     }
 }
