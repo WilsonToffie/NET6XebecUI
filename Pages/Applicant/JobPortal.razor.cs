1b7dac999c297e9f88ee94766985cc27e83f67b4
stfeleti <stfeleti@gmail.com>
2022-02-10 14:54:32 +0200

    Portals

--- Pages/Applicant/JobPortal.razor.cs
+++ Pages/Applicant/JobPortal.razor.cs
@@ -11,22 +11,21 @@ namespace XebecPortal.UI.Pages.Applicant
     {
         private string searchJob;
         private bool IsApplyHidden;
-        private bool jobPortalIsHidden = false;
-        private bool applicationFormIsHidden = true;
         private List<int> pageNum = new List<int>();
         private IList<Job> jobList = new List<Job>();
         private IList<Job> jobListFilter = new List<Job>();
         private Job displayJobDetail = new Job();
         private IPagedList<Job> jobPagedList = new List<Job>().ToPagedList();
         private IList<Application> applicationList = new List<Application>();
-        private IEnumerable<string> options { get; set; } = new HashSet<string>();
-
+        private List<JobType> JobTypes;
 
         protected override async Task OnInitializedAsync()
         {
+            JobTypes = await httpClient.GetFromJsonAsync<List<JobType>>("https://xebecapi.azurewebsites.net/api/JobType");
             jobList = await httpClient.GetFromJsonAsync<List<Job>>("https://xebecapi.azurewebsites.net/api/Job");
-            jobList = jobList.Where(x => x.Title != null && x.Company != null && x.Location != null).ToList();
             applicationList = await httpClient.GetFromJsonAsync<List<Application>>("https://xebecapi.azurewebsites.net/api/Application");
+            
+            
             jobListFilter = jobList;
             jobPagedList = jobListFilter.ToPagedList(1, 17);
             pageNum.AddRange(Enumerable.Range(1, jobPagedList.PageCount));
@@ -59,13 +58,9 @@ namespace XebecPortal.UI.Pages.Applicant
         {
             pageNum.Clear();
 
-            if (options != null && options.Count() > 0)
-                jobListFilter = jobList.Where(x => x.Location == "QC").ToList();
-
-
             if (value != null && value != "" && value != " ")
             {
-                jobListFilter = jobListFilter.Where(x => $"{x.Title} {x.Company}".Contains(value, StringComparison.OrdinalIgnoreCase)).ToList();
+                jobListFilter = jobList.Where(x => $"{x.Title} {x.Company} {x.Location}".Contains(value, StringComparison.OrdinalIgnoreCase)).ToList();
                 jobPagedList = jobListFilter.ToPagedList(1, 17);
                 pageNum.AddRange(Enumerable.Range(1, jobPagedList.PageCount));
             }
@@ -88,20 +83,5 @@ namespace XebecPortal.UI.Pages.Applicant
 
             displayJobDetail = jobListFilter.FirstOrDefault(x => x.Id == id);
         }
-
-        private void GoToApplicationForm()
-        {
-            applicationFormIsHidden = false;
-            jobPortalIsHidden = true;
-        }
-
-        private string GetStyling(Job item)
-        {
-            if (displayJobDetail.Id == item.Id)
-                return "box-shadow: rgba(0,51,64,0.86) 0px 0px 0px 2px, rgba(6, 24, 44, 0.65) 0px 4px 6px -1px, rgba(255, 255, 255, 0.08) 0px 1px 0px inset;";
-            return "";
-        }
-
-        private string GetMultiSelectionText(List<string> selectedValues) => $"Selected location{(selectedValues.Count > 1 ? "s have" : " has")}: {string.Join(", ", selectedValues.Select(x => x))}";
     }
 }
