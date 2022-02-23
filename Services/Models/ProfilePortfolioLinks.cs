8d14e92264d138a62268508a09f22889e0b947b7
Armand <armandvdberg56@gmail.com>
2022-02-23 09:06:51 +0200

    Armand

---
+++ Services/Models/ProfilePortfolioLinks.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace XebecPortal.UI.Services.Models
+{
+    public class ProfilePortfolioLink
+    {
+        public int Id { get; set; }
+        public string GitHubLink { get; set; }
+        public string LinkedInLink { get; set; }
+        public string TwitterLink { get; set; }
+        public string PersonalWebsiteUrl { get; set; }
+        public int AppUserId { get; set; }
+        public IList<AppUser> AppUser { get; set; }
+    }
+}
