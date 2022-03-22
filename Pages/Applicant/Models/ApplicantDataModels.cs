20a89d94b918adb9e2e45372593e2adf42b04bf2
Armand <armandvdberg56@gmail.com>
2022-03-22 09:01:49 +0200

    Armand

--- Pages/Applicant/Models/ApplicantDataModels.cs
+++ Pages/Applicant/Models/ApplicantDataModels.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
+using XebecPortal.UI.Pages.HR;
 
 namespace XebecPortal.UI.Pages.Applicant
 {
@@ -19,6 +20,31 @@ namespace XebecPortal.UI.Pages.Applicant
         public string phase { get; set; }
     }
 
+    public class ApplicationModel
+    {
+        [JsonProperty("id")] public int Id { get; set; }
+
+        [JsonProperty("jobId")] public int JobId { get; set; }
+
+        [JsonProperty("job")] public Job Job { get; set; }
+
+        [JsonProperty("appUserId")] public int AppUserId { get; set; }
+
+        [JsonProperty("appUser")] public AppUser AppUser { get; set; }
+
+        [JsonProperty("timeApplied")] public DateTimeOffset TimeApplied { get; set; }
+
+        [JsonProperty("beginApplication")] public DateTime BeginApplication { get; set; }
+
+        [JsonProperty("applicationPhaseId")] 
+        public int ApplicationPhaseId { get; set; }
+
+        [JsonProperty("applicationPhase")]
+        public ApplicationPhase ApplicationPhase{ get; set; }
+
+        [JsonProperty("phaseHelpers")]
+        public ApplicationPhasesHelper applicationPhasesHelper { get; set; }
+    }
     public class Job
     {
         public int Id { get; set; }
