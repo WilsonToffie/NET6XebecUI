f4cb86fe6213cfcd525e0d00846f28463c73dfdf
IviweMalotana <53525294+IviweMalotana@users.noreply.github.com>
2022-05-04 13:14:16 +0200

    All Services + Home Changes

---
+++ wwwroot/Image.js
@@ -0,0 +1,14 @@
+﻿function getImage() {
+    const imageUrl = "https://nebula123.sharepoint.com/sites/Project/Lists/Aneeqah%20List/Attachments/1/a.pdf";
+
+    (async () => {
+        const response = await fetch(imageUrl)
+        const imageBlob = await response.blob()
+        const reader = new FileReader();
+        reader.readAsDataURL(imageBlob);
+        reader.onloadend = () => {
+            const base64data = reader.result;
+            console.log(base64data);
+        }
+    })()
+}
