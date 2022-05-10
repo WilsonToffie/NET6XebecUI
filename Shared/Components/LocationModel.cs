80d19b2d037b1fcd4c3f8e30b92ef95cd9291668
IviweMalotana <53525294+IviweMalotana@users.noreply.github.com>
2022-05-10 16:39:08 +0200

    Job Portal and additional Mainlayout changes

---
+++ Shared/Components/LocationModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace XebecPortal.UI.Shared.Components
+{
+    public class LocationModel
+    {
+        public string country_name;
+        public string country_short_name;
+        public string country_phone_code;
+    }
+}
