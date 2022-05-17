02294207d07a9893b394858e5d14439d39778999
IviweMalotana <53525294+IviweMalotana@users.noreply.github.com>
2022-05-17 08:53:29 +0200

    Added Dropdown model (now fully complete)

---
+++ Shared/Components/Dropdown/DropdownModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace XebecPortal.UI.Shared.Components
+{
+    public class DropdownModel
+    {
+        public List<string> result { get; set; } 
+    }
+}
